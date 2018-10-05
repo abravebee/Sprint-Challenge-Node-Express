980fafa9da78251f293bb0c5fe61d3ab40e7fd1a
Casey Baker <caseye.baker@gmail.com>
2018-10-05 09:26:13 -0700

    CRUD operations working for projects

--- projects/projectRoutes.js
+++ projects/projectRoutes.js
@@ -25,10 +25,10 @@ const capitalize = (req, res, next) => {
 router.get('/', (req, res) => {
   projectDb.get()
     .then(projects => {
-      console.log(`\n== I found the projects!! ==\n`, projects)
+      console.log(`\n== I FOUND THESE PROJECTS!! ==\n`, projects)
       res.json(projects);
     }).catch (err => {
-      console.log(`\n== I cannot find the projects!! ==\n`, err)
+      console.log(`\n== I CANNOT FIND THE PROJECTS!! ==\n`, err)
       res.status(500)
       .send({ error: "The projects could not be retrieved." })
     })
@@ -38,20 +38,20 @@ router.get('/:id', (req, res) => {
   const { id } = req.params;
   projectDb.get(id)
     .then(project => {
-      console.log('\n== I found that project!! ==\n', project)
+      console.log('\n== I FOUND THE PROJECT!! ==\n', project)
       res.json(project);
     })
     .catch(err => {
-      console.log('\n== I cannot find that project!! ==\n', err)
+      console.log('\n== I CANNOT FIND THAT PROJECT!! ==\n', err)
       res.json({ error: "The project could not be retrieved." })
     })
 })
 
-router.get('/:id/actions', (req, res) => ) {
+router.get('/:id/actions', (req, res) => {
   const { id } = req.params;
   projectDb.getProjectActions(id)
   .then(actions => {
-    console.log('\n=== I found the actions for that project!! ==\n', actions)
+    console.log('\n=== I FOUND THE ACTIONS FOR THAT PROJECT!! ==\n', actions)
     res.json(actions);
   })
   .catch(err => {
@@ -67,14 +67,16 @@ if(!req.body.name) {
 } else if (req.body.name.length > 128) {
   return res.status(400).json({ error: "Please choose a name that is less than 128 characters."
   })
+} else if (!req.body.description) {
+  res.status(400).json({ error: "Please provide a description for this project." })
 } else {
-userDb.insert(req.body)
+projectDb.insert(req.body)
   .then(response => {
-        console.log('\n=== USER ADDED ===\n', response);
+        console.log('\n=== I SUCCESSFULLY ADDED THE PROJECT!! ===\n', response);
         res.status(201).json(response);
       })
   .catch(err => {
-    console.log('\n=== CANNOT ADD USER ==\n', err);
+    console.log('\n=== I cannot add that project!! ==\n', err);
     res.status(500).json({ error: 'There was an error while saving that user to the database.'});
   });
 }
@@ -83,45 +85,48 @@ userDb.insert(req.body)
 router.put('/:id', capitalize, (req, res) => {
 const { id } = req.params;
 if (!req.body.name) {
-  res.status(400).json({ error: "Please enter a name." })
+  res.status(400).json({ error: "Please provide a name for this project." })
 } else if (req.body.name.length > 128) {
   res.status(400).json({ error: "Please choose a name that is less than 128 characters." })
+} else if (!req.body.description) {
+  res.status(400).json({ error: "Please provide a description for this project." })
 } else {
-  userDb
+  projectDb
   .update(id, req.body)
   .then(response => {
-    if (response === 0) {
-      res.status(404).json({ error: "There is no user with that id."})
+    if (response === null) {
+      res.status(404).json({ error: "There is no project with that id."})
     }
-    if (response === 1) {
-      console.log("\n=== USER UPDATED ==\n", response, req.body)
+   else {
+      console.log("\n=== I UPDATED THE PROJECT!! ==\n", response, req.body)
       res.status(200).json(response)
     }
   })
   .catch(err => {
-    console.log("\n=== CANNOT UPDATE USER ===\n", err)
-    res.status(500).json({ error: "There was an error while updating this user." })
+    console.log("\n=== I CANNOT UPDATE THAT PROJECT!! ===\n", err)
+    res.status(500).json({ error: "There was an error while updating this project." })
   })  
 }
 })
 
-router.delete('/:id', (req, res) => {
-const id = req.params.id
-userDb.remove(id)
-  .then(response => {
-    if (response === 0){
-      res.status(404).json({ error: "There is no user with that id."})
-    }
-    if (response === 1){
-      console.log("\n=== USER DELETED ===\n", response)
-      res.status(200).json({response})
-    }
-  })
-  .catch(error => {
-    console.log(error)
-    res.status(500).json({error: "There was an error while deleting this user."})
-  })
-})
 
+router.delete('/:id', (req, res) => {
+  const id = req.params.id
+  projectDb.remove(id)
+    .then(response => {
+      if (response === 0){
+        res.status(404).json({ error: "There is no project with that id."})
+      }
+      if (response === 1){
+        console.log("\n=== I DELETED THE PROJECT!! ===\n", response)
+        res.status(200).json({response})
+      }
+    })
+    .catch(error => {
+      console.log(error)
+      res.status(500).json({error: "There was an error while deleting this user."})
+    })
+  })
+  
 
 module.exports = router;
