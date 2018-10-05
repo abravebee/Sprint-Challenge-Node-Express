2ebaa7dd5709aa7167533d4db974e11799b77818
Casey Baker <caseye.baker@gmail.com>
2018-10-05 08:44:36 -0700

    spent 20 minutes answering questions about express and forgot to install express; express installed now tho

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const logger = require('morgan');
+const cors = require('cors');
+const helmet = require('helmet');
+const port = 8000;
+
+const projectRoutes = require ('./projects/projectRoutes.js')
+const actionRoutes = require('./actions/actionRoutes.js')
+const server = express();
+
+server.use(express.json());
+server.use(cors());
+server.use(helmet());
+server.use(logger("combined"));
+
+//Routes
+server.get('/', (req, res) => {
+  res.send('<h1>Welcome to Projects</h1>')
+})
+
+server.listen(port, () =>
+console.log(`\n=== I am running the API on port ${port} ===\n`))
